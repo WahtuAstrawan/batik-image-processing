dd600efb527db26e0cdb35472b55a7d195c4bb6d
Wahtu Astrawan <chatt699@gmail.com>
2023-12-12 11:38:13 +0800

    fixing gausian blurr and edge detection method

--- generatorData.py
+++ generatorData.py
@@ -31,21 +31,21 @@ def santuration(image, dir_name):
 
 def edgeDetection(image, dir_name):
     dir_name += "_edge.jpg"
-    # Convert to graycsale
+    # Convert to graycsale for better pixel light intensity detection
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Blur the image for better edge detection
     img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
 
     # Canny Edge Detection
     edges = cv2.Canny(image=img_blur, threshold1=100,
-                      threshold2=200)  # Canny Edge Detection
+                      threshold2=200)
     cv2.imwrite(dir_name, edges)
 
 
 def gaussianBlurr(image, dir_name):
     dir_name += "_blurr.jpg"
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    denoised = cv2.medianBlur(image, 5)
+    denoised = cv2.GaussianBlur(image, (3, 3), 0)
     cv2.imwrite(dir_name, denoised)
 
 
@@ -57,7 +57,6 @@ def generate(dir_name, dir_choose, range_end, extension):
         name_file = i
         image = cv2.imread(f"./{dir_choose}/{name_file}{extension}")
         save_dir = f"{generated_dir}{i}"
-        # Apply the saturation adjustment
         santuration(image, save_dir)
         greyScale(image, save_dir)
         gaussianBlurr(image, save_dir)
